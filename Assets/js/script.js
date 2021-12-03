d10bc0586fa7695d1bd88527e8920b3c5912cc5d
glenluersman <glen.luersman@gmail.com>
2021-12-02 19:25:05 -0500

    added bg color to time blocks

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,3 +2,20 @@ var saveBtn = $(".saveBtn");
 
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 
+var timeBlock = function() {
+    var hour = moment().hours();
+
+    $(".time-block").each(function() {
+        var currentHour = parseInt($(this).attr("id"));
+        console.log(this);
+
+        if (currentHour > hour) {
+            $(this).addClass("future");
+        } else if (currentHour === hour) {
+            $(this).addClass("prestent");
+        }else {
+            $(this).addClass("past");
+        }
+    })
+
+};
